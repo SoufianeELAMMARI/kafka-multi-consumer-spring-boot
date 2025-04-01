6f7eedc978ab6dbb039a9171ff8cd71613e3931e
Soufiane ELAMMARI <soufiane.elammari@intelcia.com>
2025-04-01 17:58:39 +0000

    First commit - add order producer

---
+++ order-microservice/src/main/java/org/demo/service/producer/OrderCreatedEventProducer.java
@@ -0,0 +1,21 @@
+package org.demo.service.producer;
+
+import org.demo.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderCreatedEventProducer {
+
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    public OrderCreatedEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void produce(Order order) {
+        this.kafkaTemplate.send("order-created-event", order);
+    }
+}
---
+++ order-microservice/src/main/java/org/demo/service/testService.java
@@ -0,0 +1,4 @@
+package org.demo.service;
+
+public class testService {
+}
