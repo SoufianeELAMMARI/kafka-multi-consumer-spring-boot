83da94779b60192170f9b44a95652718103a1e0c
Soufiane ELAMMARI <soufiane.elammari@intelcia.com>
2025-04-05 15:55:22 +0000

    update project

---
+++ stock-consumer-microservice/src/main/java/org/demo/orderproducermicroservice/OrderProducerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.demo.orderproducermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProducerMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProducerMicroserviceApplication.class, args);
+    }
+
+}
---
+++ stock-consumer-microservice/src/test/java/org/demo/orderproducermicroservice/OrderProducerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.orderproducermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProducerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
